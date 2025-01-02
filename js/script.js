fd8e1d15efcc8c7baabe2e076cb109660fcbdc9a
xiaoming <2014500726@smail.xtu.edu.cn>
2025-01-02 22:40:14 +0800

    update
    Signed-off-by: xiaoming <2014500726@smail.xtu.edu.cn>

--- js/script.js
+++ js/script.js
@@ -78,7 +78,7 @@ const mainStage = new Stage("main-canvas");
 const stages = [trailsStage, mainStage];
 
 //随机文字烟花内容
-const randomWords = ["糖糖小宝", "Q❤️T", "I Love you"];
+const randomWords = ["I Love you", "糖糖小宝", "Q❤️T", "I Love you"];
 const wordDotsMap = {};
 randomWords.forEach((word) => {
 	wordDotsMap[word] = MyMath.literalLattice(word, 3, "Gabriola,华文琥珀", "90px");
